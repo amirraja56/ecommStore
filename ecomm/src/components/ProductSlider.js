f191094063ab336b336220ef0d51897b2e3134c1
amirraja56 <amirraja56@gmail.com>
2025-05-05 06:57:41 +0530

    fix add to cart issue and also design issue

--- ecomm/src/components/ProductSlider.js
+++ ecomm/src/components/ProductSlider.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { add } from '../store/cartSlice'
 import { fetchProducts } from '../store/productSlice'
@@ -11,12 +12,17 @@ import { Autoplay } from 'swiper/modules';
 const ProductSlider = () => {
   const dispatch = useDispatch();
   const { data: products } = useSelector((state) => state.product)
+  const cartItems = useSelector((state) => state.cart);
 
   const product = products.slice(7)
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [])
+
+  const isInCart = (productId) => {
+    return cartItems.some((item) => item.id === productId);
+  };
 
   const handleAdd = (product) => {
     dispatch(add(product));
@@ -36,11 +42,11 @@ const ProductSlider = () => {
         slidesPerView={6}
         spaceBetween={10}
         modules={[Autoplay]}
-        autoplay={{ delay: 1000, disableOnInteraction: false, pauseOnMouseEnter:true }}
+        autoplay={{ delay: 1000, disableOnInteraction: false, pauseOnMouseEnter: true }}
         allowTouchMove={true}
         loop
         breakpoints={{
-          200:{
+          200: {
             slidesPerView: 1,
             spaceBetween: 10,
           },
@@ -67,12 +73,31 @@ const ProductSlider = () => {
           {product.map((product) => (
             <SwiperSlide>
               <div key={product.id} className="card mt-2" style={{ width: "10rem", height: "18rem" }}>
-                <img src={product.image} className="card-img-top " style={{ height: "8rem" }} alt="..." />
-                <div className="card-body p-2 d-flex flex-column">
-                  <h5 className="card-title mt-1" style={{ fontSize: "12px" }}>{product.title?.slice(0, 50)}</h5>
-
-                  <h4 className='cart-text-bold mt-0'>&#8360; {product.price}</h4>
-                  <button onClick={() => handleAdd(product)} className="btn btn-success" style={{ position: "absolute", fontSize: "15px", marginTop: "100px", marginLeft: "20px" }}>Add to Cart</button>
+                <img src={product.image} className="card-img-top " style={{ height: "8rem",width:"8.5rem", margin: "10px" }} alt="..." />
+                <div className="card-body p-2 d-flex flex-column justify-content-between">
+                  <div style={{marginLeft:"10px"}}>
+                    <h5 className="card-title" style={{ fontSize: "12px", height: "2.5rem", overflow: "hidden", marginTop: "10px" }}>
+                      {product.title?.slice(0, 50)}
+                    </h5>
+                    <h4 className="cart-text-bold mt-1" style={{ fontSize: "20px", marginTop: "10px" }}>&#8360; {product.price}</h4>
+                  </div>
+                  {isInCart(product.id) ? (
+                    <NavLink
+                      to="/cart"
+                      className="btn btn-primary"
+                      style={{ position: "absolute", bottom: "10px", left: "10px", right: "10px", fontSize: "14px", textAlign: "center" }}
+                    >
+                      Go to Cart
+                    </NavLink>
+                  ) : (
+                    <button
+                      onClick={() => handleAdd(product)}
+                      className="btn btn-success"
+                      style={{ position: "absolute", bottom: "10px", left: "10px", right: "10px", fontSize: "14px" }}
+                    >
+                      Add to Cart
+                    </button>
+                  )}
                 </div>
               </div>
             </SwiperSlide>
